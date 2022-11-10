09705ee8fead2f440ad2c93a4e5a51fa9b208f0b
Alexis Fuentes <alexis.a.fuentes@gmail.com>
2022-11-09 20:56:53 -0600

    Configuraciones de conexión a la bd

--- backbsale/settings.py
+++ backbsale/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backbsale.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bsale_test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '192.168.2.247',
+        'PORT': '3306'
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
