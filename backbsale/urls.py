46f54288b4f54894c4d41f7e0afbd38f974dd76d
Alexis Fuentes <alexis.a.fuentes@gmail.com>
2022-11-13 16:19:28 -0600

    Agregando pantalla de inicio

--- backbsale/urls.py
+++ backbsale/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='inicio'),
     path('api/v0/', include('api_rest.urls'))
 ]
