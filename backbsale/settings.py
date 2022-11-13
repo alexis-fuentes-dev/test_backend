dab67c8453ebbd161ed33031bed3aa097576ae3d
Alexis Fuentes <alexis.a.fuentes@gmail.com>
2022-11-12 21:23:45 -0600

    Agregando control de CORS

--- backbsale/settings.py
+++ backbsale/settings.py
@@ -37,17 +37,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'api_rest'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Urls permitidas a realizar peticiones a la API
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+]
+
+# Solo permitir metodos GET para el API de prueba
+CORS_ALLOW_METHODS = [
+    "GET",
 ]
 
 ROOT_URLCONF = 'backbsale.urls'
